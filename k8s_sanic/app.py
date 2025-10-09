7b4585fb8d725e02283bc9a3e9f18e6acf877ef8
YXY1109 <1049598673@qq.com>
2025-10-09 11:45:13 +0800

    update：将sanic服务通过actions部署到k8s集群

---
+++ k8s_sanic/app.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic(__name__)
+
+
+@app.get("/")
+async def index(request):
+    # 获取当前时间的年月日时分秒
+    formatted_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return json({"message": f"Hello from Sanic on K8s!：{formatted_time}"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=False)
