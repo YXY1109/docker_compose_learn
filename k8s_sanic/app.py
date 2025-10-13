cedf71394ccc52c88cfae88df44f46f935b2e1ea
xunyang <1049598673@qq.com>
2025-10-13 15:19:39 +0800

    update：添加工作进程，修改健康检查的接口名称

--- k8s_sanic/app.py
+++ k8s_sanic/app.py
@@ -10,9 +10,14 @@ app = Sanic(__name__)
 async def index(request):
     # 获取当前时间的年月日时分秒
     formatted_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return json({"message": f"21Hello from Sanic on K8s!：{formatted_time}"})
+    return json({"message": f"22Hello from Sanic on K8s!：{formatted_time}"})
+
+
+@app.get("/health")
+async def health_check(request):
+    return json({"status": "ok"})
 
 
 if __name__ == "__main__":
     print("启动服务")
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(host="0.0.0.0", port=8000, workers=4, debug=False)
